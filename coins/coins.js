18649bab98873058cd6490757cf37cdfd94c43a3
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-04-20 15:19:02 +0200

    new changes

--- coins/coins.js
+++ coins/coins.js
@@ -1,19 +1,113 @@
-async function main() {
-    async function getCryptoPrices() {
-        return (await fetch(
-            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,USDT,BNB,USDC,XRP,ADA,DOGE,MATIC,SOL,DOT,LTC,SHIB,TRX,AVAX&tsyms=USDT"
-        )).json().then(data => data.RAW);
-    }
+const table = document.getElementById("crypto-table");
+let priceFilter = "all";
+let changeFilter = "all";
+let searchQuery = "";
 
-    function createCryptoTable(prices) {
-        const table = document.getElementById("crypto-table");
-        const header = ["Coin", "Price (USDT)", "24h Change (%)", "Symbol", "Last Market"];
-
-        table.innerHTML = "<thead><tr>" + header.map(col => "<th>" + col + "</th>").join('') + "</tr></thead><tbody>" + Object.entries(prices).map(([symbol, data]) => {
-            const cells = [data.USDT.FROMSYMBOL, data.USDT.PRICE.toFixed(2), data.USDT.CHANGEPCT24HOUR.toFixed(2), "https://cryptocompare.com/" + data.USDT.IMAGEURL, data.USDT.LASTMARKET];
-            return "<tr>" + cells.map(cell => cell.startsWith("http") ? "<td><img src='" + cell + "'></td>" : "<td>" + cell + "</td>").join('') + "</tr>";
-        }).join('') + "</tbody>";
+const fetchCryptoPrices = async () => {
+    try {
+        const response = await fetch(
+            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,USDT,BNB,USDC,XRP,ADA,DOGE,MATIC,SOL,DOT,LTC,SHIB,TRX,AVAX,DAI,LINK,UNI,ATOM,LEO,OKB,ETC,XMR,TON,ICP,XLM,BCH,FIL,TUSD,APT,HBAR,NEAR,LDO,ARB,CRO,VET,APE,ALSO,GRT,QNT,FTM,EOS,MANA,THETA,AAVE,STX,EGLD,FLOW,XTZ,AXS,RPL,USDP,SAND,IMX,BIT,CHZ,CFX,NEO,KCS,OP,SNX,CRV,RNDR,KLAY,USDD,MKR,GMX,LUNC,BSV,MINA,BTT,INJ,FXS,CAKE,ZEC,HT,DASH,XEC,XDC,MIOTA,IOTA,IOTX,CSPR,GT,TWT,PAXG,RUNE,LRC,ZIL,FLR,WOO,AGIX,DYDX,GUSD,CVX,1INCH,ENJ,KAVA,OSMO,ROSE&tsyms=USDT"
+        );
+        const data = await response.json();
+        return Object.values(data.RAW).map((coin) => {
+            return {
+                name: coin.USDT.FROMSYMBOL,
+                image: `https://www.cryptocompare.com/${coin.USDT.IMAGEURL}`,
+                current_price: coin.USDT.PRICE,
+                price_change_percentage_24h: coin.USDT.CHANGEPCT24HOUR,
+                market_cap: coin.USDT.MKTCAP,
+                total_volume: coin.USDT.TOTALVOLUME24HTO,
+            };
+        });
+    } catch (error) {
+        console.error("Error fetching crypto prices:", error);
+        return [];
     }
-    createCryptoTable(await getCryptoPrices());
-}
-main();
+};
+
+
+
+const formatPrice = (price) => {
+    return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(price);
+};
+
+const updateTable = async () => {
+    const data = await fetchCryptoPrices();
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>Symbol</th>
+          <th>Logo</th>
+          <th>Price</th>
+          <th>24h Change</th>
+          <th>Market Cap</th>
+          <th>Volume (24h)</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${data
+            .filter((coin) => {
+                console.log("priceFilter:", priceFilter);
+                console.log("changeFilter:", changeFilter);
+                console.log("coin.current_price:", coin.current_price);
+                console.log("coin.price_change_percentage_24h:", coin.price_change_percentage_24h);
+
+                if (priceFilter === "less" && coin.current_price > 10) {
+                    return false;
+                }
+                if (priceFilter === "greater" && coin.current_price <= 10) {
+                    return false;
+                }
+                if (priceFilter === "1usd" && Math.abs(coin.current_price - 1.00) > 0.01) {
+                    return false;
+                }
+                if (changeFilter === "less" && coin.price_change_percentage_24h > 0) {
+                    return false;
+                }
+                if (changeFilter === "greater" && coin.price_change_percentage_24h <= 0) {
+                    return false;
+                }
+                if (changeFilter === "nothing" && Math.abs(coin.price_change_percentage_24h) > 0.5) {
+                    return false;
+                }
+                if (searchQuery && !coin.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+                    return false;
+                }
+                return true;
+            })
+
+            .map(
+                (coin) => `
+          <tr>
+            <td>${coin.name}</td>
+            <td><img src="${coin.image}" alt="${coin.name}"></td>
+            <td>${formatPrice(coin.current_price)}</td>
+            <td>${coin.price_change_percentage_24h.toFixed(2)}%</td>
+            <td>${formatPrice(coin.market_cap)}</td>
+            <td>${formatPrice(coin.total_volume)}</td>
+          </tr>
+        `
+            )
+            .join("")}
+      </tbody>
+    `;
+};
+
+
+updateTable();
+setInterval(updateTable, 30000);
+
+document.getElementById("price-filter").addEventListener("change", (event) => {
+    priceFilter = event.target.value;
+    updateTable();
+});
+
+document.getElementById("change-filter").addEventListener("change", (event) => {
+    changeFilter = event.target.value;
+    updateTable();
+});
+
+document.getElementById("search").addEventListener("input", (event) => {
+    searchQuery = event.target.value;
+    updateTable();
+});
