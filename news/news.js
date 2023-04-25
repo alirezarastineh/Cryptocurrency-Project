83bad58173abdf8e6a63c1a8e4a738aa35c8416f
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-04-25 13:16:04 +0200

    new changes

--- news/news.js
+++ news/news.js
@@ -1,82 +1,57 @@
-//  this code retrieves news data from the API endpoint and creates individual cards for each article on the webpage. If there are any errors, it will log them to the console.
-// Uses the fetch() method to make a request to the API specified in "endpointUrl" and returns a promise object 
 async function fetchData() {
     try {
         const response = await fetch('https://min-api.cryptocompare.com/data/v2/news/?lang=EN');
         const data = await response.json();
-        createCards(data.Data); // Passes the "Data" array from the JSON response to the createCards() function
+        createCards(data.Data);
     } catch (error) {
-        console.error("error", error);
+        console.error(error);
     }
 }
 
-fetchData();
-
-// Creates card element for each article passed as an argument
 function createCard(article) {
-
-    // Creates a div element with class name "card"
     const card = document.createElement("div");
-    card.className = "card";
+    card.classList.add("card");
 
-    // Creates a h2 element for article title, adds article title text node, and appends it to card element
     const title = document.createElement("h2");
-    title.appendChild(document.createTextNode(article.title));
+    title.innerText = article.title;
     card.appendChild(title);
 
     const image = document.createElement("img");
     image.src = article.imageurl;
     card.appendChild(image);
 
-    // Creates a p element for the article body/description, adds article body text node, and appends it to card element
     const description = document.createElement("p");
-    description.appendChild(document.createTextNode(article.body));
+    description.innerText = `${article.body.slice(0, 200)}...`;
     card.appendChild(description);
 
-    // Creates a span element for read more link, adds "Show More" text node, and appends it to card element
     const readMore = document.createElement("span");
-    readMore.appendChild(document.createTextNode("Show more"));
+    readMore.innerText = "Show more";
     card.appendChild(readMore);
 
     function toggleText() {
-        if (readMore.textContent === "Show more") {
-            // If the "Show more" link is clicked, show the full article body text
+        if (readMore.innerText === "Show more") {
             description.innerText = article.body;
-            readMore.textContent = "Show less";
+            readMore.innerText = "Show less";
         } else {
-            // If the "Show less" link is clicked, hide the full article body text by removing the extra text added previously
-            description.innerText = article.body.slice(0, 200) + "...";
-            readMore.textContent = "Show more";
+            description.innerText = `${article.body.slice(0, 200)}...`;
+            readMore.innerText = "Show more";
         }
     }
 
-    // Set the initial text content for the description element to the first 200 characters of the article body
-    description.innerText = article.body.slice(0, 200) + "...";
-
-    // Add an event listener to the "Show more" link button to call the toggleText() function when clicked
     readMore.addEventListener("click", toggleText);
 
-    // Remove previous event listener before adding a new one
-    readMore.removeEventListener("click", toggleText);
-    readMore.addEventListener("click", toggleText);
-
-    // Returns completed card element with title, body/description and a "Show more/Show less" link
     return card;
 }
 
-// Defines a function called "createCards" which takes in an argument of "news"
 function createCards(news) {
-    const container = document.getElementById("cards-container");
-
-    // Creates a new empty document fragment and assigns to constant variable "fragment"
+    const container = document.querySelector("#cards-container");
     const fragment = document.createDocumentFragment();
 
-    // Iterates through each article within the news array using forEach method
-    news.forEach(function (article) {
-        // Appends the card to the fragment 
+    news.forEach((article) => {
         fragment.appendChild(createCard(article));
     });
 
-    // Appends the entire fragment (now containing all cards created) to the container element on the HTML page 
     container.appendChild(fragment);
 }
+
+fetchData();
